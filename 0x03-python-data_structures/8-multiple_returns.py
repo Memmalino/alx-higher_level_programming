b82bf660474af46bd52675eb48ffbb2072b57648
memmalino <memmalino@gmail.com>
2023-08-15 01:54:41 +0000

    hello world

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,14 @@
+def multiple_returns(sentence):
+    """calculate length of stence and first character
+
+    Args:
+        sentence: the sentence
+
+    Returns:
+        return a (length, first-char)
+    """
+    if len(sentence) == 0:
+        result = 0, None
+    else:
+        result = len(sentence), sentence[0]
+    return (result)
