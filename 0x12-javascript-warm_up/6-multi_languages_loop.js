7c03bebfae482d1733475dc08ee64d3352794604
memmalino <memmalino@gmail.com>
2023-11-13 23:56:48 +0100

    updated 6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,5 @@
 #!/usr/bin/node
 const arra = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-for (let i = 0; i < arra.length; i++)
-	console.log(arra[i])
+for (let i = 0; i < arra.length; i++) {
+  console.log(arra[i]);
+}
