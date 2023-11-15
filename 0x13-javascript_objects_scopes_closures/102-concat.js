5429e89d2aa057afed5aacd5df370c342e0dd047
memmalino <memmalino@gmail.com>
2023-11-15 05:01:02 +0100

    updated 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,18 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-fs.readFile(process.argv[2], 'utf-8', (err, content) => {
-  if (err) {
-    console.log('there is an error while reading the file');
-  } else {
-    fs.readFile(process.argv[3], 'utf-8', (err, content2) => {
-      if (err) {
-        console.log('there is an error while reading the file');
-      } else {
-        const printed = `${content}${content2}`;
-        fs.writeFile(process.argv[4], printed, err => err);
-      }
-    });
-  }
-});
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);
