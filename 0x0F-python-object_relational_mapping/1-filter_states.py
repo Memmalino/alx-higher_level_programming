0b1be52e75db7815eff15f9ed418366de72ca864
Memmalino <memmalino@gmail.com>
2023-11-17 01:11:30 +0100

    all given permission

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa"""
+"""select all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cusr = db.cursor()
-    cusr.execute("SELECT * FROM `states`")
+    cusr.execute("SELECT * FROM `states` ORDER BY `id`")
     for data in cusr.fetchall():
-        print(data)
+        if (data[1][0] == 'N'):
+            print(data)
     cusr.close()
     db.close()
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-""" lists all states from the database hbtn_0e_0_usa"""
+"""select all states from the database hbtn_0e_0_usa"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cusr = db.cursor()
-    cusr.execute("SELECT * FROM `states`")
+    cusr.execute("SELECT * FROM `states` WHERE name='{:s}'\
+            ORDER BY `id` ASC".format(sys.argv[4]))
     for data in cusr.fetchall():
         print(data)
     cusr.close()
