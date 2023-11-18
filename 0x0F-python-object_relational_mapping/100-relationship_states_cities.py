4e3786153ac699718cf62857d50b475606e8e95e
Memmalino <memmalino@gmail.com>
2023-11-18 00:10:17 +0000

    updated realtionship_state.py, relationship_city.py and 100-relationship_states_cities.py

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""Script that creates the State "California" with the City "San Francisco"
-   in the database hbtn_0e_100_usa.
-"""
-
+"""Script that creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa."""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -10,20 +7,31 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
-    db_username = sys.argv[1]
-    db_password = sys.argv[2]
-    db_name = sys.argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(db_username, db_password, db_name),
-                           pool_pre_ping=True)
+    # Check for correct number of command line arguments
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    Base.metadata.create_all(engine)
+    # Extract command line arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    session = sessionmaker(bind=engine)
-    from_session = session()
+    # Database connection setup
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database), pool_pre_ping=True)
 
+    # Bind the engine to the Base class
+    Base.metadata.create_all(bind=engine)
+
+    # Create a session
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Create a new State with a City
     new_state = State(name="California", cities=[City(name="San Francisco")])
-    from_session.add(new_state)
-    from_session.commit()
 
-    from_session.close()
+    # Add and commit the new State to the database
+    session.add(new_state)
+    session.commit()
+
+    # Close the session
+    session.close()
+
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -2,11 +2,14 @@
 """Module that contains the definition of the State class."""
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from relationship_city import Base
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 class State(Base):
     """State class that represents the states table in the database."""
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state", cascade="all, delete-orphan")
+    cities = relationship("City", back_populates="state", cascade="all, delete-orphan")
+
