c6c1d58e6e1ddf11579a80542ec4aa8fb9849114
Memmalino <memmalino@gmail.com>
2023-12-02 00:15:33 +0000

    PUSH 1-hbtn_header.py

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""
+this modeule is used to get response header-
+X-Request-Id
+"""
+
+import urllib.request
+import sys
+
+if __name__ == '__main__':
+    with urllib.request.urlopen(sys.argv[1]) as response:
+            print(response.info().get('X-Request-Id'))
