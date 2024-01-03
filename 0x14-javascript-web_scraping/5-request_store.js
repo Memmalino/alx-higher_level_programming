aea48a3a82ad01ed45074f6d8ca71872704eb36a
memmalino <memmalino@gmail.com>
2024-01-03 23:07:04 +0000

    pushed

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+const process = require('process');
+const request = require('request');
+const filesystem = require('fs');
+
+const url = process.argv[2];
+const filepath = process.argv[3];
+
+request(url, function (error, response, body) {
+  if (error != null) {
+    console.log(error);
+  } else {
+    filesystem.writeFile(filepath, body, 'utf8', function (err, data) {
+      if (err != null) {
+        console.log(error);
+      }
+    });
+  }
+});
